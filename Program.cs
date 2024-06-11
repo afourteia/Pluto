f5b36ae95b3aa4bad5955354f85dc09145124e84
Abdul Fourteia <raoph40@gmail.com>
2024-06-11 12:02:50 +0200

    request timeout

--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers(); // Add this line
+builder.Services.AddRequestTimeouts();
 var app = builder.Build();
+
+
+app.UseRequestTimeouts();
 
 app.MapGet("/", () => "Main page. YOLO!");
 app.MapControllers(); // Add this line
